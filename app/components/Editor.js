446203bbcc5fa55dccae76e4d3afa0142f472ee4
Mark <muchirimark2@gmail.com>
2023-09-25 12:11:25 +0300

    null

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React from "react";
 import ReactMde from "react-mde";
 import Showdown from "showdown";
@@ -6,43 +6,43 @@ import propTypes from "prop-types";
 import "react-mde/lib/styles/css/react-mde-all.css";
 
 function Editor({ tempNoteText, setTempNoteText }) {
-    // Define PropTypes for the component's props
-    Editor.propTypes = {
-        tempNoteText: propTypes.object, // Expecting an object for currentNote
-        setTempNoteText: propTypes.func, // Expecting a function for updateNote
-    };
+	// Define PropTypes for the component's props
+	Editor.propTypes = {
+		tempNoteText: propTypes.object, // Expecting an object for currentNote
+		setTempNoteText: propTypes.func, // Expecting a function for updateNote
+	};
 
-    // Initialize the selectedTab state to "write"
-    const [ selectedTab, setSelectedTab ] = React.useState("write");
+	// Initialize the selectedTab state to "write"
+	const [ selectedTab, setSelectedTab ] = React.useState("write");
 
-    // Create a Showdown converter for rendering Markdown
-    const converter = new Showdown.Converter({
-        tables: true,
-        simplifiedAutoLink: true,
-        strikethrough: true,
-        tasklists: true,
-    });
+	// Create a Showdown converter for rendering Markdown
+	const converter = new Showdown.Converter({
+		tables: true,
+		simplifiedAutoLink: true,
+		strikethrough: true,
+		tasklists: true,
+	});
 
-    return (
-        <section className="pane-editor">
-            <ReactMde
-                // Set the initial value to the tempNoteText for debouncing
-                value={tempNoteText}
-                // function to update the note content to the debounced data
-                onChange={setTempNoteText}
-                // Selected tab for write/preview
-                selectedTab={selectedTab}
-                // Callback to change the selected tab
-                onTabChange={setSelectedTab}
-                // Function to generate a Markdown preview
-                generateMarkdownPreview={(markdown) =>
-                    Promise.resolve(converter.makeHtml(markdown))
-                }
-                // Set the minimum editor height and height units
-                minEditorHeight={80}
-                heightUnits="vh"
-            />
-        </section>
-    );
+	return (
+		<section className="pane-editor">
+			<ReactMde
+				// Set the initial value to the tempNoteText for debouncing
+				value={tempNoteText}
+				// function to update the note content to the debounced data
+				onChange={setTempNoteText}
+				// Selected tab for write/preview
+				selectedTab={selectedTab}
+				// Callback to change the selected tab
+				onTabChange={setSelectedTab}
+				// Function to generate a Markdown preview
+				generateMarkdownPreview={(markdown) =>
+					Promise.resolve(converter.makeHtml(markdown))
+				}
+				// Set the minimum editor height and height units
+				minEditorHeight={80}
+				heightUnits="vh"
+			/>
+		</section>
+	);
 }
 export default Editor;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-'use client';
+"use client";
 import { Karla } from "next/font/google";
 import React from "react";
 import Sidebar from "./components/Sidebar";
@@ -10,141 +10,141 @@ import { notesCollection, db } from "./firebase.js";
 
 // Global fonts
 const karla = Karla({
-  subsets: [ 'latin', 'latin-ext' ],
-  weight: [ '400', '700' ],
+	subsets: [ "latin", "latin-ext" ],
+	weight: [ "400", "700" ],
 });
 
 function Home() {
+	// Define state variables for notes and the current selected note ID
+	const [ notes, setNotes ] = React.useState([]);
+	const [ currentNoteId, setCurrentNoteId ] = React.useState("");
+	const [ tempNoteText, setTempNoteText ] = React.useState("");
 
-  // Define state variables for notes and the current selected note ID
-  const [ notes, setNotes ] = React.useState([]);
-  const [ currentNoteId, setCurrentNoteId ] = React.useState("");
-  const [ tempNoteText, setTempNoteText ] = React.useState("");
+	// Find the currently selected note based on its ID or default to the first note
+	const currentNote =
+		notes.find((note) => note.id === currentNoteId) || notes[ 0 ];
 
-  // Find the currently selected note based on its ID or default to the first note
-  const currentNote =
-    notes.find((note) => note.id === currentNoteId) || notes[ 0 ];
+	// Set up an effect to listen for changes in the notes collection from Firebase
+	React.useEffect(() => {
+		const unsubscribe = onSnapshot(notesCollection, function (snapshot) {
+			// Sync up our local notes array with the snapshot data from Firebase
+			const notesArr = snapshot.docs.map((doc) => ({
+				...doc.data(),
+				id: doc.id,
+			}));
+			// Sorts the notes (from: latest to oldest) as they sync
+			const sortedNotes = notesArr.sort(
+				(a, b) => b.updatedAt - a.updatedAt
+			);
+			setNotes(sortedNotes);
+		});
+		// Unsubscribe when the component unmounts to prevent memory leaks
+		return unsubscribe;
+	}, []);
 
-  // Set up an effect to listen for changes in the notes collection from Firebase
-  React.useEffect(() => {
-    const unsubscribe = onSnapshot(notesCollection, function (snapshot) {
-      // Sync up our local notes array with the snapshot data from Firebase
-      const notesArr = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      // Sorts the notes (from: latest to oldest) as they sync
-      const sortedNotes = notesArr.sort(
-        (a, b) => b.updatedAt - a.updatedAt
-      );
-      setNotes(sortedNotes);
-    });
-    // Unsubscribe when the component unmounts to prevent memory leaks
-    return unsubscribe;
-  }, []);
+	// Set up an effect to update text on the edito to the currentNote state
+	React.useEffect(() => {
+		if (currentNote) {
+			setTempNoteText(currentNote.body);
+		}
+	}, [ currentNote ]);
 
-  // Set up an effect to update text on the edito to the currentNote state
-  React.useEffect(() => {
-    if (currentNote) {
-      setTempNoteText(currentNote.body);
-    }
-  }, [ currentNote ]);
+	// Debounce with setTimeout JS func.
+	// Important:  This is what connects to firestor for the updates
+	React.useEffect(() => {
+		const timeoutId = setTimeout(() => {
+			// So when I click a note it doesn't update the tempNoteText
+			// And so only update if the condition is true
+			if (tempNoteText !== currentNote?.body) {
+				updateNote(tempNoteText);
+			}
+			// Uncomment 👇 for testing puporses only
+			// updateNote(tempNoteText);
+		}, 500);
+		return () => clearTimeout(timeoutId);
+	}, [ tempNoteText ]);
 
-  // Debounce with setTimeout JS func.
-  // Important:  This is what connects to firestor for the updates
-  React.useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      // So when I click a note it doesn't update the tempNoteText
-      // And so only update if the condition is true
-      if (tempNoteText !== currentNote?.body) {
-        updateNote(tempNoteText);
-      }
-      // Uncomment 👇 for testing puporses only
-      // updateNote(tempNoteText);
-    }, 500);
-    return () => clearTimeout(timeoutId);
-  }, [ tempNoteText ]);
+	// Set up an effect to initialize the currentNoteId if it's not set
+	React.useEffect(() => {
+		if (!currentNoteId) {
+			setCurrentNoteId(notes[ 0 ]?.id);
+		}
+	}, [ notes, currentNoteId ]);
 
-  // Set up an effect to initialize the currentNoteId if it's not set
-  React.useEffect(() => {
-    if (!currentNoteId) {
-      setCurrentNoteId(notes[ 0 ]?.id);
-    }
-  }, [ notes, currentNoteId ]);
+	// Function to create a new note
+	async function createNewNote() {
+		const newNote = {
+			body: "# Type your markdown note's title here",
+			createdAt: Date.now(),
+			updatedAt: Date.now(),
+		};
+		// Add a new note document to the notes collection in Firebase
+		const newNoteRef = await addDoc(notesCollection, newNote);
+		setCurrentNoteId(newNoteRef.id);
+	}
 
-  // Function to create a new note
-  async function createNewNote() {
-    const newNote = {
-      body: "# Type your markdown note's title here",
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    };
-    // Add a new note document to the notes collection in Firebase
-    const newNoteRef = await addDoc(notesCollection, newNote);
-    setCurrentNoteId(newNoteRef.id);
-  }
+	// Function to update the content of the current note
+	async function updateNote(text) {
+		/**
+		 * Note => Added the if condition
+		 * This modification ensures that you only attempt to create a
+		 * document reference and update the document if currentNoteId
+		 * is defined, preventing the "Invalid document reference" error.
+		 */
+		if (currentNoteId) {
+			const docRef = doc(db, "notes", currentNoteId);
+			// Update the note document in Firebase with the new content
+			await setDoc(
+				docRef,
+				{ body: text, updatedAt: Date.now() },
+				{ merge: true }
+			);
+		}
+	}
 
-  // Function to update the content of the current note
-  async function updateNote(text) {
-    /**
-     * Note => Added the if condition
-     * This modification ensures that you only attempt to create a 
-     * document reference and update the document if currentNoteId 
-     * is defined, preventing the "Invalid document reference" error.
-     */
-    if (currentNoteId) {
-      const docRef = doc(db, "notes", currentNoteId);
-      // Update the note document in Firebase with the new content
-      await setDoc(
-        docRef,
-        { body: text, updatedAt: Date.now() },
-        { merge: true }
-      );
-    }
-  }
+	// Function to delete a note
+	async function deleteNote(noteId) {
+		const docRef = doc(db, "notes", noteId);
+		// Delete the note document from the notes collection in Firebase
+		await deleteDoc(docRef);
+	}
 
+	// <main className={karla.className}>
 
-  // Function to delete a note
-  async function deleteNote(noteId) {
-    const docRef = doc(db, "notes", noteId);
-    // Delete the note document from the notes collection in Firebase
-    await deleteDoc(docRef);
-  }
-
-  return (
-    <main className={karla.className}>
-      {
-        // Check if there are any notes to display
-        notes.length > 0 ? (
-          // Split the UI into Sidebar and Editor components
-          <Split
-            sizes={[ 30, 70 ]}
-            direction="horizontal"
-            className="split"
-          >
-            <Sidebar
-              notes={notes}
-              currentNote={currentNote}
-              setCurrentNoteId={setCurrentNoteId}
-              newNote={createNewNote}
-              deleteNote={deleteNote}
-            />
-            <Editor
-              tempNoteText={tempNoteText}
-              setTempNoteText={setTempNoteText}
-            />
-          </Split>
-        ) : (
-          // Display a message when there are no notes
-          <div className="no-notes">
-            <h1>You have no notes</h1>
-            <button className="first-note" onClick={createNewNote}>
-              Create one now
-            </button>
-          </div>
-        )
-      }
-    </main>
-  );
+	return (
+		<main className={karla.className}>
+			{
+				// Check if there are any notes to display
+				notes.length > 0 ? (
+					// Split the UI into Sidebar and Editor components
+					<Split
+						sizes={[ 30, 70 ]}
+						direction="horizontal"
+						className="split"
+					>
+						<Sidebar
+							notes={notes}
+							currentNote={currentNote}
+							setCurrentNoteId={setCurrentNoteId}
+							newNote={createNewNote}
+							deleteNote={deleteNote}
+						/>
+						<Editor
+							tempNoteText={tempNoteText}
+							setTempNoteText={setTempNoteText}
+						/>
+					</Split>
+				) : (
+					// Display a message when there are no notes
+					<div className="no-notes">
+						<h1>You have no notes</h1>
+						<button className="first-note" onClick={createNewNote}>
+							Create one now
+						</button>
+					</div>
+				)
+			}
+		</main>
+	);
 }
 export default Home;
