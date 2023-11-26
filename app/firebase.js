43cc4fc26b5273ee535f2781268d4fe54e158e64
Mark <muchirimark2@gmail.com>
2023-11-26 03:53:28 +0300

    removed analytics

--- app/firebase.js
+++ app/firebase.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getAnalytics } from 'firebase/firestore';
+import { getFirestore, collection } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,5 +23,3 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 // Access to the notes collection
 export const notesCollection = collection(db, 'notes');
-// analytics
-const analytics = getAnalytics(app);
